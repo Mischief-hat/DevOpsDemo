d3f97847cc1187614ba7d46523e0f0f5dc5362a5
Brijesh Giri <Brijesh.Giri@gep.com>
2019-08-16 20:29:26 +0530

    DotNetCore Console App

---
+++ ConsoleDemo/ConsoleDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello to the world of dotnet core!");
+            Console.WriteLine("You are in for a ride");
+        }
+    }
+}
